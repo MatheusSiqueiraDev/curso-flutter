7c7c8a1af9b5c5acb219628e6db8be8d0957e9c0
Matheus Siqueira <siqueira4503@gmail.com>
2022-09-09 19:40:20 -0300

    subindo novas aulas

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+main () {
+  runApp(PerguntaApp());
+}
+
+@override
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá, Flutter!!!'),
+    )
+  }
+}
